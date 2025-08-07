6662ad006ff2690476b02e58555d334800c4fe5a
terraquad <tq@terraquad.dev>
2025-08-07 13:03:22 +0200

    Run update.sh, update to Zig master, change header access to behave like other deps

--- build.zig
+++ build.zig
@@ -1,3 +1,24 @@
 const std = @import("std");
 
-pub fn build(_: *std.Build) void {}
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const module = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+    });
+    module.addCSourceFile(.{
+        .file = b.addWriteFiles().add("empty.c", ""),
+    });
+
+    const lib = b.addLibrary(.{
+        .name = "wayland-headers",
+        .root_module = module,
+        .linkage = .static,
+    });
+    lib.installHeadersDirectory(b.path("wayland"), "wayland", .{});
+    lib.installHeadersDirectory(b.path("wayland-protocols"), "wayland-protocols", .{});
+    lib.installHeadersDirectory(b.path("libdecor"), "libdecor", .{});
+    b.installArtifact(lib);
+}
